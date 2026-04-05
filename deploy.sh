741a2903359f5633d79c1cd434d9a45cb40223cc
vanvj <vanvj@vanvjdeMac-mini.local>
2026-04-05 18:49:51 +0800

    deploy: 2026-04-05 18:49:50

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-REPO_DIR="/Users/vanvj/my-blog"
-REMOTE_URL="https://github.com/vanvj00003/my-blog"
-
-commit_msg="${1:-deploy: $(date '+%Y-%m-%d %H:%M:%S')}"
+REPO_DIR="${PWD}"
+COMMIT_MSG="${1:-deploy: $(date '+%Y-%m-%d %H:%M:%S')}"
+REMOTE_URL="https://vanvj00003@github.com/vanvj00003/my-blog.git"
+GH_PAGES_BRANCH="gh-pages"
 
 echo "=========================================="
-echo "开始部署博客到 GitHub"
+echo "发布博客到 https://vanvj00003.github.io/my-blog/"
 echo "=========================================="
 
 cd "$REPO_DIR"
@@ -24,13 +24,13 @@ hugo --minify
 # Add changes
 echo "[3/4] 提交更改..."
 git add -A
-git commit -m "$commit_msg" || echo "没有更改需要提交"
+git commit -m "$COMMIT_MSG" || echo "没有更改需要提交"
 
 # Push to gh-pages
-echo "[4/4] 推送到 GitHub gh-pages..."
-git push "$REMOTE_URL" gh-pages
+echo "[4/4] 推送到 gh-pages..."
+git push ${REMOTE_URL:-origin} ${GH_PAGES_BRANCH}
 
 echo "=========================================="
-echo "部署完成！"
-echo "博客地址：https://vanvj00003.github.io/monkvan"
+echo "发布完成！"
+echo "博客地址：https://vanvj00003.github.io/my-blog/"
 echo "=========================================="
